83107903610791cef1be111d6c2b2843bdd0b861
Matthieu Jimenez <matthieu@jimenez.lu>
2017-08-02 16:29:16 +0200

    Test + bug fix

--- src/test/java/gat/bitmap/CTBitmapTest.java
+++ src/test/java/gat/bitmap/CTBitmapTest.java
@@ -2,6 +2,7 @@ package gat.bitmap;
 
 import gat.AdditionalTypesPlugin;
 import gat.MockStorage;
+import gat.bitset.CTBitset;
 import greycat.Graph;
 import greycat.GraphBuilder;
 import greycat.Node;
@@ -13,13 +14,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CTBitmapTest {
-    private Graph graph;
+    private Graph g;
     private Node n;
 
     @BeforeEach
-    public void initGraph(){
+    public void initGraph() {
         MockStorage storage = new MockStorage();
-        Graph g = GraphBuilder.newBuilder()
+        g = GraphBuilder.newBuilder()
                 .withPlugin(new AdditionalTypesPlugin())
                 .withStorage(storage)
                 .withScheduler(new NoopScheduler())
@@ -29,13 +30,49 @@ class CTBitmapTest {
     }
 
     @Test
-    public void addEmpty(){
+    public void cardinality() {
+        CTBitmap bitset = (CTBitmap) n.getOrCreateCustom("bitset", CTBitmap.NAME);
+        bitset.add(0);
+        bitset.add(10);
+        bitset.add(17);
+        bitset.add(2);
+        bitset.add(4);
+
+        assertEquals(5,bitset.cardinality());
 
     }
 
+    @Test
+    public void reload() {
+        CTBitmap bitset = (CTBitmap) n.getOrCreateCustom("bitset", CTBitmap.NAME);
+        bitset.add(0);
+        bitset.add(10);
+        bitset.add(17);
+        bitset.add(2);
+        bitset.add(4);
+
+        bitset.save();
+        CTBitmap bitset2 = (CTBitmap) n.getOrCreateCustom("bitset", CTBitmap.NAME);
+
+        assertArrayEquals(bitset.getBitMap().toArray(),bitset2.getBitMap().toArray());
+        assertEquals(bitset.cardinality(),bitset2.cardinality());
+    }
+
+    @Test
+    public void next() {
+        CTBitmap bitset = (CTBitmap) n.getOrCreateCustom("bitset", CTBitmap.NAME);
+        bitset.add(0);
+        bitset.add(10);
+        bitset.add(17);
+        bitset.add(2);
+        bitset.add(4);
+        assertEquals(0,bitset.nextSetBit(0));
+        assertEquals(17,bitset.nextSetBit(11));
+    }
+
     @AfterEach
-    public void close(){
+    public void close() {
         n.free();
-        graph.disconnect(null);
+        g.disconnect(null);
     }
 }
--- src/test/java/gat/bitset/CTBitsetTest.java
+++ src/test/java/gat/bitset/CTBitsetTest.java
@@ -13,13 +13,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CTBitsetTest {
-    private Graph graph;
+    private Graph g;
     private Node n;
 
     @BeforeEach
-    public void initGraph(){
+    public void initGraph() {
         MockStorage storage = new MockStorage();
-        Graph g = GraphBuilder.newBuilder()
+        g = GraphBuilder.newBuilder()
                 .withPlugin(new AdditionalTypesPlugin())
                 .withStorage(storage)
                 .withScheduler(new NoopScheduler())
@@ -29,13 +29,49 @@ class CTBitsetTest {
     }
 
     @Test
-    public void addEmpty(){
+    public void cardinality() {
+        CTBitset bitset = (CTBitset) n.getOrCreateCustom("bitset", CTBitset.NAME);
+        bitset.add(0);
+        bitset.add(10);
+        bitset.add(17);
+        bitset.add(2);
+        bitset.add(4);
+
+        assertEquals(5,bitset.cardinality());
 
     }
 
+    @Test
+    public void reload() {
+        CTBitset bitset = (CTBitset) n.getOrCreateCustom("bitset", CTBitset.NAME);
+        bitset.add(0);
+        bitset.add(10);
+        bitset.add(17);
+        bitset.add(2);
+        bitset.add(4);
+
+        bitset.save();
+        CTBitset bitset2 = (CTBitset) n.getOrCreateCustom("bitset", CTBitset.NAME);
+
+        assertArrayEquals(bitset.getBitset().toIntArray(),bitset2.getBitset().toIntArray());
+        assertEquals(bitset.cardinality(),bitset2.cardinality());
+    }
+
+    @Test
+    public void next() {
+        CTBitset bitset = (CTBitset) n.getOrCreateCustom("bitset", CTBitset.NAME);
+        bitset.add(0);
+        bitset.add(10);
+        bitset.add(17);
+        bitset.add(2);
+        bitset.add(4);
+        assertEquals(0,bitset.nextSetBit(0));
+        assertEquals(17,bitset.nextSetBit(11));
+    }
+
     @AfterEach
-    public void close(){
+    public void close() {
         n.free();
-        graph.disconnect(null);
+        g.disconnect(null);
     }
 }
